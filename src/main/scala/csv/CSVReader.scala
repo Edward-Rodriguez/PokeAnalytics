224af1faa3d3c33be90c1b90766b36482afab492
Edward Rodriguez <edwardR36@hotmail.com>
2020-12-11 15:44:25 -0500

    change array datastructure to map

--- src/main/scala/csv/CSVReader.scala
+++ src/main/scala/csv/CSVReader.scala
@@ -1,10 +1,23 @@
 package csv
-
+import scala.collection.mutable.Map
 object CSVReader extends App {
 
+  val m = Map[String, Int]();
+  val z = Map[String, Int]();
+
   val file = io.Source.fromFile("zillow.csv")
-  for (line <- file.getLines()) {
-    val cols = line.split(",").map(_.trim)
-    println(s"${cols(2)} | ${cols(4)} | ${cols(5)} | ${cols(6)}")
+  for (line <- file.getLines().drop(1)) {
+
+    var numOfBeds = line.split(",")(2)
+    var zipCode = line.split(",")(4)
+    var year = line.split(",")(5)
+    var price = line.split(",")(6)
+
+    if (m.contains(numOfBeds)) {
+      m(numOfBeds) += 1
+    } else {
+      m(numOfBeds) = 1
+    }
   }
+  m.foreach(println)
 }
