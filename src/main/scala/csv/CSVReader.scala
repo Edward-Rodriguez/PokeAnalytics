db0d23c715bdb32893e873ae7a94bf16da786e16
Edward Rodriguez <edwardR36@hotmail.com>
2020-12-13 19:21:17 -0500

    Add Pokemon case class Add pokemon csv Add cli flag 'pokemon type' feature

--- src/main/scala/csv/CSVReader.scala
+++ src/main/scala/csv/CSVReader.scala
@@ -1,23 +1,77 @@
 package csv
 import scala.collection.mutable.Map
+import scala.collection.mutable.Buffer
 object CSVReader extends App {
 
-  val m = Map[String, Int]();
-  val z = Map[String, Int]();
+  case class Pokemon(
+      pokedexNumber: Int,
+      name: String,
+      type1: String,
+      type2: String,
+      totalStat: Int,
+      hp: Int,
+      attack: Int,
+      defense: Int,
+      specialAttack: Int,
+      specialDefense: Int,
+      speed: Int,
+      gen: Int,
+      legendary: Boolean
+  )
 
-  val file = io.Source.fromFile("zillow.csv")
-  for (line <- file.getLines().drop(1)) {
+  val file = io.Source.fromFile("Pokemon.csv")
 
-    var numOfBeds = line.split(",")(2)
-    var zipCode = line.split(",")(4)
-    var year = line.split(",")(5)
-    var price = line.split(",")(6)
-
-    if (m.contains(numOfBeds)) {
-      m(numOfBeds) += 1
-    } else {
-      m(numOfBeds) = 1
-    }
+  /** convert string array (representing one record from csv)
+    * into a tuple (pokedex#, Pokemon)
+    * to be stored in Map later on
+    *
+    * @param record
+    * @return
+    */
+  def convertRecordToPokemon(record: Array[String]): (Int, Pokemon) = {
+    record(0).toInt -> Pokemon(
+      record(0).toInt, // pokedexNumber
+      record(1), // name
+      record(2), // type1
+      record(3), // type 2
+      record(4).toInt, // total
+      record(5).toInt, // hp
+      record(6).toInt, // attack
+      record(7).toInt, // defense
+      record(8).toInt, // specialAttack
+      record(9).toInt, // specialDefense
+      record(10).toInt, // speed
+      record(11).toInt, // generation
+      record(12).toBoolean // legendary
+    )
   }
-  m.foreach(println)
+
+  val pokeMap =
+    file
+      .getLines()
+      .drop(1)
+      .map(line => convertRecordToPokemon(line.split(",")))
+      .toMap
+
+  /** WORKING PROGRESS ...
+    *
+    * @param flag
+    * @param data
+    * @return
+    */
+  def searchPokemonForType(
+      flag: String,
+      data: scala.collection.immutable.Map[Int, Pokemon]
+  ): scala.collection.immutable.Map[Int, Pokemon] = {
+    data
+      .filter(x =>
+        (x._2.type1.toLowerCase == flag || x._2.type2.toLowerCase == flag)
+      )
+      .toMap
+  }
+
+  val newMap = searchPokemonForType(args(0).toLowerCase(), pokeMap)
+  if (!newMap.isEmpty) newMap.foreach(x => println(x._2.name))
+  else "No Pokemon of that type sorry . . ."
+
 }
